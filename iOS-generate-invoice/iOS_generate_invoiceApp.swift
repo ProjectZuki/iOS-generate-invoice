b08c0a29eb5a6429b5fca773db9cf956283e6050
Willie Alcaraz <63901327+ProjectZuki@users.noreply.github.com>
2024-05-17 03:28:54 -0700

    Initial Commit

---
+++ iOS-generate-invoice/iOS_generate_invoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_generate_invoiceApp.swift
+//  iOS-generate-invoice
+//
+//  Created by Willie Alcaraz on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_generate_invoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
